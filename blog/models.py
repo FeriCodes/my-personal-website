71f3ef677a5952f4461d8bb3508e8465f2a8c35c
FeriCodes <hosseinbigdeli335@gmail.com>
2026-09-21 21:05:50 +0330

    feat: add Task model for My Tasks and apply migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from datetime import datetime
 
-# Create your models here.
+
+class Task(models.Model):
+    STATUS_CHOICE = [('pending', 'Pending'), ('done', 'Done'), ('frozen', 'Frozen'), ('broken', 'Streak Broken')]
+
+    name = models.CharField(max_length=100, unique=True)
+    streak = models.IntegerField(default=0)
+    longest_streak = models.IntegerField(default=0)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICE, default='pending')
+    last_updated = models.DateTimeField(null=True, blank=True)
+    freezes_left = models.IntegerField(default=3)
+    last_freeze_reset = models.CharField(max_length=7, default=timezone.now().strftime("%Y-%m"))
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.last_freeze_reset:
+            self.last_freeze_reset = datetime.now().strftime("%Y-%m")
+        super().save(*args, **kwargs)
