24778cb6f6129d0f1d894ba7e9f9c94cb8971e7b
FeriCodes <hosseinbigdeli335@gmail.com>
2026-09-22 15:17:56 +0330

    feat: implement backend logic and URL endpoints for routines

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path('routines/', views.routines_view, name='routines'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.tasks import Task
+from .models import Task
 
 
 def home(request):
     return render(request, "blog/home.html")
+
+
+def routines_view(request):
+    tasks = Task.objects.all()
+    return render(request, 'blog/routines.html', {'tasks': tasks})
